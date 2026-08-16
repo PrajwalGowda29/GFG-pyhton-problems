45b800b77079bb4709a5da58d021300873479fd7
PrajwalGowda29 <prajwal.brb.24.beai@acharya.ac.in>
2026-08-16 23:01:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Product Subset/min-product-subset.py
@@ -0,0 +1,15 @@
+class Solution:
+  def minProd(self, arr):
+    n = len(arr)
+    ans = float('inf')
+
+    for mask in range(1, 1 << n):
+        product = 1
+
+        for i in range(n):
+            if mask & (1 << i):
+                product *= arr[i]
+
+        ans = min(ans, product)
+
+    return ans
